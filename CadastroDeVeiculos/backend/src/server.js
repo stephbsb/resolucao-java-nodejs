6a09deee441ce4ef3a3f33285af24a9e6226a38f
stephbsb <stephany0303@gmail.com>
2020-09-06 12:30:15 -0300

    criacao do server usando expressjs

---
+++ CadastroDeVeiculos/backend/src/server.js
@@ -0,0 +1,52 @@
+// API entrada da api
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./database/connection');
+
+const veiculosRoutes = require('./routes/veiculosRoutes');
+
+const app = express();
+
+// transforma o corpo para formato json
+app.use(bodyParser.json());
+
+// headers para lidar com erro de CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With, Content-Type, Accept,Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+
+  next();
+});
+
+// middleware that filters the url to the right routes
+app.use('/veiculos', veiculosRoutes);
+
+// Se rota nao existe retorna um erro
+app.use((req, res, next) => {
+  const error = new Error('Rota nao encontrada');
+  error.statusCode = 404;
+  return next(error);
+});
+
+// retorna mensagens de erro para o usuário
+app.use((error, req, res, next) => {
+  console.log(error);
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  console.log(error.message);
+  res.status(error.code || 500);
+  res.json({ error: error.message || 'Um erro ocorreu.' });
+});
+
+// a aplicação irá ouvir todos os requests na porta 5000
+app.listen(5000);
