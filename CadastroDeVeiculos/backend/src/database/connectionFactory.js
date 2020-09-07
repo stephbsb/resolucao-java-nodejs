37260c5c0e2064c3a87eafd546dac6241aed9df9
stephbsb <stephany0303@gmail.com>
2020-09-07 16:22:40 -0300

    adiciona constraints e chech ao criar tabela

--- CadastroDeVeiculos/backend/src/database/connectionFactory.js
+++ CadastroDeVeiculos/backend/src/database/connectionFactory.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const sqlite3 = require('sqlite3');
 const path = require('path');
 const DB_PATH = path.resolve(__dirname, 'database.db');
@@ -7,29 +9,29 @@ const db = new sqlite3.Database(DB_PATH, function (err) {
     console.log(err);
     return;
   }
-  createTable();
+
   console.log('Connected to ' + DB_PATH + ' database.');
 });
 
-const createTable = () => {
-  const sql = `
+const sql = `
       CREATE TABLE IF NOT EXISTS veiculo (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        veiculo STRING,
-        marca STRING,
-        ano INTEGER,
-        descricao TEXT,
-        vendido BOOL,
-        created DATETIME,
-        updated DATETIME
+        veiculo STRING NOT NULL,
+        marca STRING NOT NULL,
+        ano INTEGER NOT NULL,
+        descricao TEXT NOT NULL,
+        vendido BOOL NOT NULL,
+        created DATETIME NOT NULL,
+        updated DATETIME NOT NULL
+
+        CHECK ((vendido == 0 OR vendido == 1) AND length(marca) >= 3 AND length(veiculo) >= 3 AND length(descricao) >= 3 AND ano >= 1900 AND ano <= 2050)
       )
       `;
 
-  db.run(sql, function (err) {
-    if (err) {
-      console.log('Erro ao criar a tabela Veiculos');
-    }
-  });
-};
+db.run(sql, function (err) {
+  if (err) {
+    console.log('Erro ao criar a tabela Veiculos');
+  }
+});
 
 module.exports = db;
