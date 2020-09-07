743f3f19c799f89467556008680f03474666a0c6
stephbsb <stephany0303@gmail.com>
2020-09-06 23:37:02 -0300

    funcao para validar grafia de marcas

---
+++ CadastroDeVeiculos/backend/src/utils/validateMarca.js
@@ -0,0 +1,35 @@
+const marcas = [
+  'Chevrolet',
+  'Volkswagen',
+  'Fiat',
+  'Renault',
+  'Ford',
+  'Toyota',
+  'Hyundai',
+  'Jeep',
+  'Honda',
+  'Nissan',
+  'Citroen',
+  'Mitsubish',
+  'Peugeot',
+  'Chery',
+  'BMW',
+  'Mercedes-Benz',
+  'Kia',
+  'Audi',
+  'Volvo',
+  'Land Rover',
+];
+
+const validateMarca = (marca) => {
+  if (marcas.indexOf(marca) === -1) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+module.exports = {
+  marcas,
+  validateMarca,
+};
