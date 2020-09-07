fbd4a8878f41f31ef6fc87af710a45be53e34873
stephbsb <stephany0303@gmail.com>
2020-09-07 16:20:56 -0300

    documentacao

--- CadastroDeVeiculos/backend/src/utils/veiculoToPE.js
+++ CadastroDeVeiculos/backend/src/utils/veiculoToPE.js
@@ -1,3 +1,11 @@
+/**
+ * Altera o formato do objeto veiculo para ser passado para o usuário.
+ * No banco de dados SQlite3 o tipo booleano de dados é salvo como 0 ou 1,
+ * Nesta função o veiculo transforma em true ou false, e retorna o objeto
+ * modificado
+ * @param {Object} veiculo - Objeto veículo em seu formato original
+ * @return {Object} veiculo - Objeto veiculo modificado para UI
+ */
 const veiculoToPE = (veiculo) => {
   if (veiculo.vendido === 1) {
     veiculo.vendido = true;
